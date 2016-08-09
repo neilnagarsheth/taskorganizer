a7ab97488c0a35fc568365c725e0c7dfc09f676a
neilnagarsheth <neilnagarsheth@gmail.com>
2016-08-08 21:11:37 -0400

    adding a service call for a single task

--- taskorganizer-frontend/app/task/task.service.ts
+++ taskorganizer-frontend/app/task/task.service.ts
@@ -10,8 +10,17 @@ import 'rxjs/Rx';
 export class TaskService{
 	constructor (private http: Http){}
 	private serverURL = "http://localhost:1337/";
+	private getTaskURL = "getTask/";
 	private getTasksURL = "getTasks/";
 	private updateTaskURL = "updateTask/";
+	private createTaskURL = "createTask/";
+
+	getTask(id: number): Observable<Task>{
+		return this.http
+				    .get(this.serverURL + this.getTaskURL + id)
+					.map(response => response.json())
+					.catch(this.handleError);
+	}
 
 	getTasks(): Observable<Task[]> {
 		return this.http
@@ -24,6 +33,14 @@ export class TaskService{
 		let body = JSON.stringify(task);
 		return this.http
 				    .put(this.serverURL + this.updateTaskURL + task.id, body)
+					.map(response => response.json())
+					.catch(this.handleError);
+	}
+
+	createTask(task: Task): Observable<Task[]>{
+		let body = JSON.stringify(task);
+		return this.http
+					.post(this.serverURL + this.createTaskURL, body)
 					.map(response => response.json())
 					.catch(this.handleError);
 	}
