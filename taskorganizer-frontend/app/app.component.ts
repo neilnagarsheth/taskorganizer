1ec72bdf4e89dfd45e0e415872e0eb84a7d3dcc2
neilnagarsheth <neilnagarsheth@gmail.com>
2016-08-08 21:10:12 -0400

    adding frontend routing to indivual taska and overall tasks

--- taskorganizer-frontend/app/app.component.ts
+++ taskorganizer-frontend/app/app.component.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { TasksComponent } from './task/task-component/task.component';
+import { TaskDetailComponent } from './task/task-detail-component/task-detail.component';
 import { TaskService } from './task/task.service';
 
 @Component({
     selector: 'task-organizer',
-    template: '<task></task>',
+    template: '<router-outlet></router-outlet>',
 	providers: [
 		ROUTER_PROVIDERS,
 		TaskService
 	],
-	directives: [TasksComponent]
+	directives: [TasksComponent, ROUTER_DIRECTIVES]
 })
+
+@RouteConfig([
+  {
+    path: '/tasks',
+    name: 'Tasks',
+    component: TasksComponent,
+    useAsDefault: true
+  },
+
+  {
+    path: '/taskdetails/:id',
+    name: 'TaskDetails',
+    component: TaskDetailComponent
+  }
+])
+
 export class AppComponent { 
 	
 }
